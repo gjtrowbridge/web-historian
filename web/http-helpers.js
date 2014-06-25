290ff65d04173f0b56e293e759346df92023f410
Hack Reactor Students <gjtrowbridge@gmail.com>
2014-06-25 15:51:06 -0700

    Implemented serveAssets

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -3,16 +3,42 @@ var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 
 exports.headers = headers = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10, // Seconds.
-  'Content-Type': "text/html"
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'text/html'
 };
 
-exports.serveAssets = function(res, asset) {
-  // Write some code here that helps serve up your static files!
-  // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+//Render the given asset (only renders assets that are in the public directory)
+exports.serveAssets = function(response, asset) {
+  //Only serves assets in 'public' directory
+  var filename = archive.paths.siteAssets + '/' + asset;
+
+  //Checks whether the asset passed has a valid path
+  path.exists(filename, function(exists) {
+    //If not, 404
+    if(!exists) {
+      response.writeHead(404, {'Content-Type': 'text/plain'});
+      response.write('404 Not Found\n');
+      response.end();
+      return;
+    }
+
+    //If so, render the asset
+    fs.readFile(filename, 'binary', function(err, file) {
+      if(err) {
+        response.writeHead(500, {'Content-Type': 'text/plain'});
+        response.write(err + '\n');
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, 'binary');
+      response.end();
+    });
+  });
 };
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,26 @@
 var path = require('path');
-var archive = require('../helpers/archive-helpers');
+var archive = require('../helpers/archive-helpers.js');
+var helpers = require('./http-helpers.js');
+var url = require('url');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+//Handles incoming server requests
+exports.handleRequest = function (request, response) {
+  // res.end(archive.paths.list);
+
+  //Get URI from URL
+  var uri = url.parse(request.url).pathname;
+
+  //Only handles root urls
+  if (uri === '/') {
+    //Render the home page (index.html)
+    helpers.serveAssets(response, 'index.html');
+
+  //Otherwise send 404 error
+  } else {
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.write("404 Not Found\n");
+    response.end();
+    return;
+  }
 };
